c15c0fbd582a63b3ebbbbc4bba562f5d0967e351
tangxiangong <tangxiangong@gmail.com>
2025-04-08 08:07:34 +0800

    feat: integrate weather service into std_io module
    - Replace calculator service with Weather service for weather information retrieval
    - Implement get_weather_info method to fetch weather data based on latitude and longitude
    - Update server function to serve Weather service and handle errors appropriately

--- src/std_io.rs
+++ src/std_io.rs
@@ -1,7 +1,49 @@
-pub mod calculator;
-
-use rmcp::{ServiceExt, transport::stdio};
+use crate::weather::{Point, WeatherClient};
+use rmcp::{
+    ServerHandler, ServiceExt,
+    model::{ServerCapabilities, ServerInfo},
+    schemars, tool,
+    transport::stdio,
+};
 use tracing_subscriber::{self, EnvFilter};
+
+#[derive(Debug, Clone)]
+pub struct Weather;
+
+#[tool(tool_box)]
+impl Weather {
+    #[tool(description = "Get the weather information of a specific location")]
+    async fn get_weather_info(
+        &self,
+        #[tool(param)]
+        #[schemars(description = "the latitude of the location")]
+        latitude: f64,
+        #[tool(param)]
+        #[schemars(description = "the longitude of the location")]
+        longitude: f64,
+    ) -> String {
+        let point = Point {
+            latitude,
+            longitude,
+        };
+        let weather = WeatherClient::default();
+        weather
+            .info(&point)
+            .await
+            .unwrap_or("Failed to get weather information".to_string())
+    }
+}
+
+#[tool(tool_box)]
+impl ServerHandler for Weather {
+    fn get_info(&self) -> ServerInfo {
+        ServerInfo {
+            instructions: Some("A simple weather information provider".to_string()),
+            capabilities: ServerCapabilities::builder().enable_tools().build(),
+            ..Default::default()
+        }
+    }
+}
 
 pub async fn server() -> anyhow::Result<()> {
     // Initialize the tracing subscriber with file and stdout logging
@@ -14,12 +56,9 @@ pub async fn server() -> anyhow::Result<()> {
     tracing::info!("Starting MCP server");
 
     // Create an instance of our counter router
-    let service = calculator::Calculator
-        .serve(stdio())
-        .await
-        .inspect_err(|e| {
-            tracing::error!("serving error: {:?}", e);
-        })?;
+    let service = Weather.serve(stdio()).await.inspect_err(|e| {
+        tracing::error!("serving error: {:?}", e);
+    })?;
 
     service.waiting().await?;
     Ok(())
